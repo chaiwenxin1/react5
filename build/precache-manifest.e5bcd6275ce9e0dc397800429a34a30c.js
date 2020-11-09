4dc6548371150c5cd2f82b4e77890229a19bf826
chaiwenxin1 <2249816238@qq.com>
2020-11-09 19:29:58 +0800

    work5

---
+++ build/precache-manifest.e5bcd6275ce9e0dc397800429a34a30c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c011865cffbb434b6b1f1dfdc04532ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "041b3621eb1ff8c022b5",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "67861f6a5378e8265bcb",
+    "url": "/static/css/main.33c841f6.chunk.css"
+  },
+  {
+    "revision": "041b3621eb1ff8c022b5",
+    "url": "/static/js/2.b8a221bb.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.b8a221bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67861f6a5378e8265bcb",
+    "url": "/static/js/main.8990524b.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
